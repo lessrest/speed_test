3117a85d304d8e57a4518933502c03416445d068
Nathn Johnson <nathanjohnson320@gmail.com>
2020-03-08 17:12:54 -0400

    Adding license and links fields

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule SpeedTest.MixProject do
   def project do
     [
       app: :speed_test,
+      description: "Package that helps automate browser testing, similar to puppeteer.",
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/nathanjohnson320/speed_test"}
     ]
   end
 
@@ -24,9 +27,9 @@ defmodule SpeedTest.MixProject do
     [
       {:elixir_uuid, "~> 1.2"},
       {:chroxy, "~> 0.6.3"},
-      {:chrome_remote_interface, "~> 0.4.1", override: true},
+      {:chrome_remote_interface, "~> 0.3.0"},
       {:plug_cowboy, "~> 2.0.1"},
-      {:plug, "~> 1.7.2", override: true}
+      {:plug, "~> 1.8.0"}
     ]
   end
 end
